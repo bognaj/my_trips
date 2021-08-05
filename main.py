864f7e85c6d768bb2cfd819611e99c40447d89c0
bogna <bodzia590@gmail.com>
2021-08-05 17:48:12 +0200

    points on map, more tracks

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def process_gpx_to_df(directory, file_name):
     data = []
     segment_length = segment.length_3d()
     for point_idx, point in enumerate(segment.points):
-        data.append([point.longitude, point.latitude,point.elevation, point.time, segment.get_speed(point_idx)])
+        try:
+            data.append([point.longitude, point.latitude,point.elevation, point.time, segment.get_speed(point_idx)])
+        except:
+            data.append([point.longitude, point.latitude, 0, 0, 0])
     columns = ['Longitude', 'Latitude', 'Altitude', 'Time', 'Speed']
     gpx_df = pd.DataFrame(data, columns=columns)
  
@@ -27,7 +30,7 @@ def process_gpx_to_df(directory, file_name):
 
 if __name__ == "__main__":
     lat, lon = 50.325625, 16.944352
-    zoom_start = 4
+    zoom_start = 5
     dir_mount = 'gpx_files_mountains'
     dir_run = 'gpx_files_running'
     dir_walk = 'gpx_files_walking'
@@ -48,6 +51,12 @@ if __name__ == "__main__":
         if file.endswith(".gpx"):
             df, points = process_gpx_to_df(dir_walk, file)
             folium.PolyLine(points, color='green', weight=4.5, opacity=.5).add_to(m)
+
+    places_df = pd.read_csv('interesting_places.csv', header = 0, encoding= 'unicode_escape')
+
+    for index, row in places_df.iterrows():
+        tooltip = row["place"]
+        folium.Marker([row["lat"], row["lon"]], popup="<b>" + str(row["place"]) + "</b>" + "<br>" + str(row["region"]), icon=folium.Icon(icon="heart"), tooltip=tooltip).add_to(m)
 
     m.save('index.html')
 
